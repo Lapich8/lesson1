0c7d509800cae7e336ce38a391972cd3e3e5ee84
Lapich8 <dima.lapich@gmail.com>
2020-04-22 14:06:48 +0300

    omfg

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/Lapich8/lesson1.git
+
+go 1.14
+
+require rsc.io/quote v1.5.2
---
+++ main.go
@@ -0,0 +1,12 @@
+cat <<EOF > hello.go
+package main
+
+import (
+    "fmt"
+    "rsc.io/quote"
+)
+
+func main() {
+    fmt.Println(quote.Hello())
+}
+EOF
